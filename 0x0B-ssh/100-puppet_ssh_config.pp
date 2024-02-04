0646b264974ba0cf9a85a5ffa5a9ade814ac78ac
Paul Wainaina <wainainapaul6880@gmail.com>
2024-02-04 11:44:26 -0800

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,14 +1,16 @@
 #!/usr/binenv bash
 #using puppet to make changes to configuration file
 
-file {	'ect/ssh/ssh_config':
+file {	'ect/ssh/ssh_cofig':
 	ensure => present,
-content => "
+
+content =>"
 
 	#ssh client configuration
 	host*
 	IdentityFile ~/.ssh/school
 	PasswordAuthentication no
 	",
+
 }
 
